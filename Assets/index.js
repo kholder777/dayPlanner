df1517246bcb2cc8006cfd0486f4f637e4e6658d
Kevin Holder <kholder777@yahoo.com>
2020-05-18 23:26:30 -0500

    added for loop.

--- Assets/index.js
+++ Assets/index.js
@@ -2,3 +2,7 @@
 $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
 // var let & const info
+var alltextarea = $("textarea");
+for (var i = 0; i < alltextarea.length; i++) {
+	return;
+}
