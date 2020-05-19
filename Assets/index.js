90482c56b664e53ef71df1ac196359a1f327920b
Kevin Holder <kholder777@yahoo.com>
2020-05-19 00:30:35 -0500

    created loop to generate proper colors to each row at the propper time.

--- Assets/index.js
+++ Assets/index.js
@@ -3,6 +3,18 @@ $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
 // var let & const info
 var alltextarea = $("textarea");
+
 for (var i = 0; i < alltextarea.length; i++) {
-	return;
+	var currentHour = parseInt(moment().format("HH"));
+	// var currentHour = 09;
+	var targetTextArea = alltextarea[i].getAttribute("data-value");
+	if (targetTextArea < currentHour) {
+		$(alltextarea[i]).addClass("past");
+	} else if (targetTextArea > currentHour) {
+		$(alltextarea[i]).addClass("future");
+	} else {
+		$(alltextarea[i]).addClass("present");
+	}
 }
+
+// save button to local storage
