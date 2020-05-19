615e27e4b8bff1575901309d506b868beb34a4f0
Kevin Holder <kholder777@yahoo.com>
2020-05-19 15:32:30 -0500

    brought readme up to date, also modified the index.js to pull from local storage.

--- Assets/index.js
+++ Assets/index.js
@@ -1,5 +1,5 @@
 // date and time
-$("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+$("#currentDay").text(moment().format("MMMM Do YYYY"));
 
 // var let & const info
 var alltextarea = $("textarea");
@@ -7,10 +7,10 @@ var alltextarea = $("textarea");
 for (var i = 0; i < alltextarea.length; i++) {
 	var currentHour = parseInt(moment().format("HH"));
 	// var currentHour = 09;
-	var targetTextArea = alltextarea[i].getAttribute("data-value");
-	if (targetTextArea < currentHour) {
+	var textAreaHour = alltextarea[i].getAttribute("data-value");
+	if (textAreaHour < currentHour) {
 		$(alltextarea[i]).addClass("past");
-	} else if (targetTextArea > currentHour) {
+	} else if (textAreaHour > currentHour) {
 		$(alltextarea[i]).addClass("future");
 	} else {
 		$(alltextarea[i]).addClass("present");
@@ -33,11 +33,19 @@ $(document).ready(function () {
 function storeTxtArea() {
 	for (let i = 0; i < alltextarea.length; i++) {
 		localStorage.setItem(
-			alltextarea[i].getAttribute("id"),
-			alltextarea[i].value
+			$(alltextarea[i]).attr("id"),
+			// alltextarea[i].getAttribute("id"),
+			$(alltextarea[i]).val()
+			// alltextarea[i].value
 		);
-		// const element = alltextarea[time];
 	}
-	alltextarea;
-	// If storedevent + each text area
+}
+
+renderTxtArea();
+
+function renderTxtArea() {
+	for (let i = 0; i < alltextarea.length; i++) {
+		storedItem = localStorage.getItem($(alltextarea[i]).attr("id"));
+		$(alltextarea[i]).val(storedItem);
+	}
 }
--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # 05 Third-Party APIs: Work Day Scheduler
 
+Below are the requirements for this project.
+I opted to put the save button inside the block that contains the time, so the user does not have to travel across the page to click save. also, this allows for more horizontal space to be filled with information.
+
 Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
 
 You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
@@ -38,9 +41,10 @@ The following animation demonstrates the application functionality:
 
 You are required to submit the following for review:
 
-* The URL of the deployed application.
+- The URL of the deployed application.
 
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
+- The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
 
-- - -
+---
+
 © 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
